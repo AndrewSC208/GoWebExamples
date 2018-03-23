0b2372cee75e87307fc9f026f5f1313c1682fcb6
Andrew Meiling <ameiling@circadence.com>
2018-03-23 06:00:07 -0700

    serve static assets

---
+++ StaticFiles/StaticFiles.go
@@ -0,0 +1,14 @@
+/**
+ * ASSETS AND FILES
+ * This will show you how to serve static assets and files
+ */
+package main
+
+import "net/http"
+
+func main() {
+	fs := http.FileServer(http.Dir("assets/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":8080", nil)
+}
